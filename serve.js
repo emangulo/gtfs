843005093c711f169f3f019ad7b506fa45ff05ee
Elias Angulo <emangulo1@gmail.com>
2026-01-17 19:09:29 -0800

    Add cors

--- serve.js
+++ serve.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from "cors";
 import { getStationData } from "./modules/gtfs.js";
 
 let PORT = process.env.PORT || 3000;
 
 let app = express();
 
+app.use(cors());
+
 app.get("/:stationID", async (req, res) => {
   try {
     let stationData = await getStationData(req.params.stationID);
+    console.log(stationData);
     res.send(stationData);
 
     let now = new Date(Date.now());
