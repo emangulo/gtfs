4843d74279b1b7b65725575d478203ddb1c1dc0c
Elias Angulo <emangulo1@gmail.com>
2026-01-25 10:24:44 -0800

    Add API key functionality

--- serve.js
+++ serve.js
@@ -1,11 +1,24 @@
-import express, { response } from "express";
+import express from "express";
 import { getStationData } from "./modules/gtfs-live.js";
 import { getStationSchedule } from "./modules/gtfs-static.js";
 
 let PORT = process.env.PORT || 3000;
-
 let app = express();
 
+const API_KEY = process.env.API_KEY;
+
+let checkAPIKey = (req, res, next) => {
+  const apiKey = req.query.api_key;
+
+  if (apiKey === API_KEY) {
+    next();
+  } else {
+    res.status(403).send("Invalid API Key");
+  }
+};
+app.use(checkAPIKey);
+
+// Routes Here
 app.get("/live/:stationID", async (req, res) => {
   try {
     let stationData = await getStationData(req.params.stationID);
