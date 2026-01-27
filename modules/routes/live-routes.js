573cbb77ef7644fedc83c56fa7a44db4503cec6e
Elias Angulo <emangulo1@gmail.com>
2026-01-26 20:59:31 -0800

    Add message for blank response (#3)

--- modules/routes/live-routes.js
+++ modules/routes/live-routes.js
@@ -6,7 +6,11 @@ import { getStationData } from "../gtfs-live.js";
 routerLive.get("/:stationID", async (req, res) => {
   try {
     let stationData = await getStationData(req.params.stationID);
-    res.send(stationData);
+    if (stationData == []) {
+      res.send("No scheduled trains");
+    } else {
+      res.send(stationData);
+    }
   } catch (error) {
     console.error(error);
   }
