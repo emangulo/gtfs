5bb04ef2e0291c4749d02e67861075c0043cbe58
Elias Angulo <emangulo1@gmail.com>
2026-02-14 11:04:19 -0800

    rearrange modules structure

--- modules/routes/static-routes.js
+++ modules/routes/static-routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 let router = express.Router();
 
-import { getStationSchedule } from "../gtfs-static.js";
+import { getStationSchedule } from "../data/gtfs-static.js";
 
 router.get("/:stationID", async (req, res) => {
   try {
